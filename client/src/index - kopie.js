f9118ff0b416780307255525352f4f0c61b0116b
doa-b <doa.bonifacio@gmail.com>
2019-07-06 12:40:43 +0200

    added rest of the files

--- client/src/index - kopie.js
+++ client/src/index - kopie.js
@@ -7,47 +7,47 @@ import { reducer as form } from 'redux-form';
 import { Route, Switch } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import {
-    ConnectedRouter,
-    connectRouter,
-    routerMiddleware
+  ConnectedRouter,
+  connectRouter,
+  routerMiddleware
 } from 'connected-react-router';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import * as serviceWorker from './serviceWorker';
 // Import your reducers and routes here
 import task from './reducers/task/';
-import tasklist from './reducers/tasklist/';
-
 import taskRoutes from './routes/task';
+import tasklist from './reducers/tasklist/';
 import tasklistRoutes from './routes/tasklist';
 
 import Welcome from './Welcome';
 
 const history = createBrowserHistory();
 const store = createStore(
-    combineReducers({
-        router: connectRouter(history),
-        form,
-        task,
-        tasklist
-        /* Add your reducers here */
-    }),
-    applyMiddleware(routerMiddleware(history), thunk)
+  combineReducers({
+    router: connectRouter(history),
+    form,
+    /* Add your reducers here */
+    task,
+    tasklist
+  }),
+  applyMiddleware(routerMiddleware(history), thunk)
 );
 
 ReactDOM.render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <Switch>
-                <Route path="/" component={Welcome} strict={true} exact={true}/>
-                {taskRoutes}
-                {tasklistRoutes}
-                {/* Add your routes here */}}
-                <Route render={() => <h1>Not Found</h1>} />
-            </Switch>
-        </ConnectedRouter>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={Welcome} strict={true} exact={true}/>
+        {/* Add your routes here */
+            taskRoutes,
+            tasklistRoutes
+        }
+        <Route render={() => <h1>Not Found</h1>} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
